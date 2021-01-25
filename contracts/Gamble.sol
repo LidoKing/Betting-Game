4cd7b6ae83293518df0ff311791fbb8afbd587bb
LidoKing <kinglido203@gmail.com>
2021-01-25 16:09:14 +0800

    Added get randomness function and overrode fulfillment function

--- contracts/Gamble.sol
+++ contracts/Gamble.sol
@@ -8,16 +8,24 @@ contract Gamble is VRFConsumerBase {
   uint256 public randomNumber;
   bytes32 internal keyHash;
   uint256 internal fee;
-  
+
   constructor()
-      VRFConsumerBase(
-          0xf720CF1B963e0e7bE9F58fd471EFa67e7bF00cfb, // VRF Coordinator
-          0x20fE562d797A42Dcb3399062AE9546cd06f63280  // LINK Token
-      ) public
+    VRFConsumerBase(
+      0xf720CF1B963e0e7bE9F58fd471EFa67e7bF00cfb, // VRF Coordinator
+      0x20fE562d797A42Dcb3399062AE9546cd06f63280  // LINK Token
+    ) public
   {
-      keyHash = 0xced103054e349b8dfb51352f0f8fa9b5d20dde3d06f9f43cb2b85bc64b238205;
-      fee = 0.1 * 10 ** 18; // 0.1 LINK
+    keyHash = 0xced103054e349b8dfb51352f0f8fa9b5d20dde3d06f9f43cb2b85bc64b238205;
+    fee = 0.1 * 10 ** 18; // 0.1 LINK
   }
 
+  function getRandomNumber(uint256 userProvidedSeed) public returns (bytes32 requestId) {
+    require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK - fill contract with faucet");
+    return requestRandomness(keyHash, fee, userProvidedSeed);
+  }
 
+  function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
+    reqId = requestId;
+    randomNumber = randomness;
+  }
 }
